82aecc373417a292411ad9818c1dbc1655ece24e
Bisc <80872017+BiscDaBiscuit@users.noreply.github.com>
2022-05-25 16:07:42 -0700

    Old Account System Deleted And More

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 import os
 from flask_sqlalchemy import SQLAlchemy
 from time import sleep
 
 app = Flask(__name__)
-app.secret_key = os.getenv("secret_key")
+#app.secret_key = os.getenv("secret_key")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.permanent_session_lifetime = timedelta(days=30)
+#app.permanent_session_lifetime = timedelta(days=30)
 db = SQLAlchemy(app)
 
 
@@ -28,6 +30,10 @@ def links():
 @app.route('/comingsoon')
 def comingsoon():
   return render_template('comingsoon.html')
+
+@app.route('/comingsomeday')
+def comingsomeday():
+  return render_template('comingsomeday.html')
 
 db.create_all()
 app.run(host='0.0.0.0', port=8080, debug=True)
