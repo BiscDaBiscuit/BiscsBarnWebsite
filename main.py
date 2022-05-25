08be20a009296e8571d05bb4d17c2b1cc4928079
Bisc <80872017+BiscDaBiscuit@users.noreply.github.com>
2022-05-25 16:18:33 -0700

    Quick Changes

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
 import os
-from flask_sqlalchemy import SQLAlchemy
 from time import sleep
 
 app = Flask(__name__)
-#app.secret_key = os.getenv("secret_key")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.permanent_session_lifetime = timedelta(days=30)
-db = SQLAlchemy(app)
-
-
 
 @app.route('/home')
 def home():
@@ -35,5 +25,4 @@ def comingsoon():
 def comingsomeday():
   return render_template('comingsomeday.html')
 
-db.create_all()
 app.run(host='0.0.0.0', port=8080, debug=True)
